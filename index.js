947c1851daf368a06465bb06fa45cfdf2483e26e
Julie C <jucheng925@gmail.com>
2023-05-10 09:53:17 -0400

    my solution

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i = 0; i < array.length; i++) {
+    for(let j = i +1; j < array.length; j++) {
+      let sum = array[i] + array[j]
+      if (sum === target){
+        return true
+      }
+    }
+  }
+  return false
 }
 
 /* 
@@ -8,10 +17,13 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  Take the first number and the second number of the array - sum it up and see if equal the target. If not, use the first and third number and then compared. etc. 
+  until reach the last number on the second constant, then the first constant need to be the second number. in otherword - should be a nested function
 */
 
 /*
   Add written explanation of your solution here
+  expect true when two numbers in the array have a sum that is equal to the target number, expect false when no combination of number in the array sum equal the target
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +41,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+  
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([65, 20, 14, 6, 22], 28));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([0, 5, 3, 6, 100], 25));
 }
 
 module.exports = hasTargetSum;
